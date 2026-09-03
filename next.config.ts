ece812c2bec9ad7205feff6cb012e889fe5c7142
rishipavanr <rishipavan248@gmail.com>
2026-09-03 23:15:16 +0530

    Nessage

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // Add this line to make it compatible with Cloudflare
+  output: "standalone",
+  // ... rest of your config
+};
 
 export default nextConfig;
