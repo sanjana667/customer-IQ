6fc8f45da45fc0a2d2e0b306bebc256124da6f6d
rishipavanr <rishipavan248@gmail.com>
2026-09-04 08:40:40 +0530

    Remove export config for Cloudflare

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,8 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/dev/types/routes.d.ts";
+import type { NextConfig } from "next";
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+const nextConfig: NextConfig = {
+  // Remove "output: export" completely
+  // Just keep your other config if any
+};
+
+export default nextConfig;
